7f0fafbd6deff712dc25617329766d3f49e810eb
vsdum <danmarkhunter13072006@gmail.com>
2022-07-06 22:31:05 +0300

    Removed magic damage hook causing many EntityHurts

--- src/native/kernelex/modules/callbacks.cpp
+++ src/native/kernelex/modules/callbacks.cpp
@@ -34,19 +34,6 @@ void KEXCallbacksModule::initialize() {
     );
 
     HookManager::addCallback(
-        SYMBOL("mcpe", "_ZN3Mob11hurtEffectsERK17ActorDamageSourceibb"),
-        LAMBDA((HookManager::CallbackController* controller, Mob* mob, const ActorDamageSource& source, int value, bool b1, bool b2), {
-            jlong victim = mob->getUniqueID()->id;
-            VTABLE_FIND_OFFSET(ActorDamageSource_isEntitySource, _ZTV17ActorDamageSource, _ZNK17ActorDamageSource14isEntitySourceEv);
-            jlong attacker = VTABLE_CALL<bool>(ActorDamageSource_isEntitySource, (ActorDamageSource*) &source) ? ((ActorDamageByActorSource*) &source)->entityUid.id : -1ll;
-            jint type = (int) source.getCause();
-            JavaCallbacks::invokeControlledCallback("onEntityHurt", "(JJIIZZ)V", controller, JavaCallbacks::PREVENTABLE, victim, attacker, type, value, b1, b2);
-            return false;
-        }, ),
-        HookManager::CALL | HookManager::LISTENER | HookManager::CONTROLLER | HookManager::RESULT
-    );
-
-    HookManager::addCallback(
         SYMBOL("mcpe", "_ZN21ActorEventCoordinator27sendActorCarriedItemChangedER5ActorRK12ItemInstanceS4_8HandSlot"),
         LAMBDA((ActorEventCoordinator* coordinator, Actor& actor, const ItemInstance& oldItem, const ItemInstance& newItem, HandSlot hand), {
             JavaCallbacks::invokeCallback(KEXJavaBridge::Cache::CallbacksModule(), "onChangeCarriedItem", "(JJJB)V", actor.getUniqueID()->id, (jlong) &oldItem, (jlong) &newItem, (jbyte) hand);
