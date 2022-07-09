78d5ef1993d3faf9c65d7c0e95f2fe670440fb05
vsdum <danmarkhunter13072006@gmail.com>
2022-07-09 15:06:20 +0300

    Added GlobalContext.getLevel()

--- src/java/kernelex/src/vsdum/kex/natives/GlobalContext.java
+++ src/java/kernelex/src/vsdum/kex/natives/GlobalContext.java
@@ -5,12 +5,20 @@ import android.support.annotation.Nullable;
 public class GlobalContext {
     
     private static native long nativeGetLocalPlayer();
+    private static native long nativeGetLevel();
 
     @Nullable public static final LocalPlayer getLocalPlayer()
     {
         long playerPtr = nativeGetLocalPlayer();
         if(playerPtr == 0L) return null;
         return new LocalPlayer(playerPtr, true);
+    }
+
+    @Nullable public static final Level getLevel()
+    {
+        long levelPtr = nativeGetLevel();
+        if(levelPtr == 0L) return null;
+        return new Level(levelPtr);
     }
 
 }
--- src/native/kernelex/classes/GlobalContext.cpp
+++ src/native/kernelex/classes/GlobalContext.cpp
@@ -3,9 +3,17 @@
 #include <innercore/global_context.h>
 
 
-extern "C" JNIEXPORT jlong JNICALL Java_vsdum_kex_natives_GlobalContext_nativeGetLocalPlayer
-(JNIEnv*, jclass) {
-    LocalPlayer* player = GlobalContext::getLocalPlayer();
-    if(player == nullptr) return 0;
-    return (jlong) player;
+extern "C" {
+    JNIEXPORT jlong JNICALL Java_vsdum_kex_natives_GlobalContext_nativeGetLocalPlayer
+    (JNIEnv*, jclass) {
+        LocalPlayer* player = GlobalContext::getLocalPlayer();
+        if(player == nullptr) return 0;
+        return (jlong) player;
+    }
+    JNIEXPORT jlong JNICALL Java_vsdum_kex_natives_GlobalContext_nativeGetLevel
+    (JNIEnv*, jclass) {
+        Level* level = GlobalContext::getLevel();
+        if(level == nullptr) return 0;
+        return (jlong) level;
+    }
 }
